6f2f7a6f7a3e08cd8f0ff512a83540fb72fee4e0
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-07-22 14:44:42 +0700

    Fix tests error

--- cli/src/commands/run.js
+++ cli/src/commands/run.js
@@ -2,7 +2,7 @@ import log from '../utils/logger';
 
 exports.command = 'run';
 exports.desc = 'Initiate checks';
-exports.builder = {}
+exports.builder = {};
 exports.handler = async function () {
 	log.info('Use --help argument to get available subcommands');
 };
